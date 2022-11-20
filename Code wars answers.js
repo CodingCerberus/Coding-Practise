841b69fe54c36d7af498a7fc7c18330bac117951
Jackson Payne <codingcerberus@gmail.com>
2022-11-20 23:50:01 +0100

    new answers

--- Code wars answers.js
+++ Code wars answers.js
@@ -128,7 +128,16 @@ function descendingOrder(n){
 }
 
 
-//
+//Growth of a Population
+
+function nbYear(p0, percent, aug, p) {
+  let n = 0;
+  while (p0 < p){
+    p0 = p0 * percent / 100 + aug;
+    n++
+  };
+return n
+}
 
 
 
