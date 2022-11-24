ea48b420244f62fd38feefe41a8de20482e431d9
Jackson Payne <codingcerberus@gmail.com>
2022-11-24 23:53:48 +0100

    new answers

--- Code wars answers.js
+++ Code wars answers.js
@@ -114,6 +114,46 @@ function maps(x){
 }
 
 
+//Convert a Boolean to a String
+
+function booleanToString(b){
+  return String(b)
+}
+
+//String repeat
+
+function repeatStr (n, s) {
+  s = s.repeat(n);
+  return s
+}
+
+//Fake Binary
+
+function fakeBin(x){
+  x = x.replace(/1/g, "0");
+ x = x.replace(/2/g, "0");
+  x = x.replace(/3/g, "0");
+  x = x.replace(/4/g, "0");
+  x = x.replace(/5/g, "1");
+  x = x.replace(/6/g, "1");
+  x = x.replace(/7/g, "1");
+  x = x.replace(/8/g, "1");
+  x = x.replace(/9/g, "1");
+  return x
+}
+
+//Count by X
+
+function countBy(x, n) {
+  let z = [];
+  for (i = 1; i <= n; i++){
+    z.push(i*x);
+  } 
+  return z;
+}
+
+
+
 /////////////////////////////
   //Kyu 7
 /////////////////////////////
@@ -206,6 +246,5 @@ function openOrSenior(data){
   }
   return outputA
 }
-
 
 
