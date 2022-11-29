ac78e201d2540fe26f3d27ff901c082ff410f70d
Jackson Payne <codingcerberus@gmail.com>
2022-11-29 23:48:18 +0100

    new answers

--- Code wars answers.js
+++ Code wars answers.js
@@ -361,6 +361,21 @@ function reverseWords(str) {
 }
 
 
+//Printer Errors 
+///regex
+
+function printerError(s) {
+  s = s.split('');
+  let errorC = 0;
+  for (i = 0; i < s.length; i++){
+    if (s[i].match(/[n-z]/)){
+      errorC++
+    }
+  }
+  return `${errorC}/${s.length}`
+}
+
+
 //Kyu 6
 
 //Counting Duplicates
