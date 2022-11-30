88ce1e6c3a2c28a3a3ce7d780dbeb3467562ef40
Jackson Payne <codingcerberus@gmail.com>
2022-11-30 23:14:36 +0100

    new answers

--- Code wars answers.js
+++ Code wars answers.js
@@ -190,6 +190,17 @@ function invert(array) {
    return invertArr;
 }
 
+
+//Grasshopper - Summation
+
+var summation = function (num) {
+  let result = num;
+  for (i = 1; i < num; i++){
+    result += i
+  }
+  return result
+  }
+
 /////////////////////////////
   //Kyu 7
 /////////////////////////////
