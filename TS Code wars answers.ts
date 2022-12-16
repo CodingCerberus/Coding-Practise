967e97d8629f2c6c8d6d01b6fd82beb5fb75b7ae
Jackson Payne <codingcerberus@gmail.com>
2022-12-16 23:31:28 +0100

    new answers

--- TS Code wars answers.ts
+++ TS Code wars answers.ts
@@ -95,4 +95,24 @@ export function bmi(weight: number, height: number): string {
   } else if (bmiCalc > 30){
     return "Obese"
   }
+  throw new Error("The method or operation is not implemented.");
 }
+
+//should work, but doesn't
+////except I tried it again, and now it does...
+////it onl works if the throw new Error is in here at the end????
+
+export function bmi(weight: number, height: number): string {
+  let bmiCalc = weight/height**2
+  if (bmiCalc <= 18.5){
+    return "Underweight"
+  } else if (bmiCalc <= 25){
+    return "Normal"
+  } else if (bmiCalc <= 30){
+    return "Overweight"
+  } else {
+    return "Obese"
+  }
+}
+
+//this works fine so the only difference is the last else if?
