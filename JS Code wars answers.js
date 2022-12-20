cfc9bace1e9bccd894c1729a21768e76ee991838
Jackson Payne <codingcerberus@gmail.com>
2022-12-20 23:25:02 +0100

    new answers

--- JS Code wars answers.js
+++ JS Code wars answers.js
@@ -431,6 +431,28 @@ var number=function(array){
 }
 
 
+//Beginner Series #3 Sum of Numbers
+
+function getSum( a,b ){
+  if (a == b){
+    return a
+  } else if (a > b){
+    let result = 0
+    for (i = b; i <= a; i++){
+      result += i
+      }
+    return result
+    } else {
+    let result = 0
+    for (i = a; i <= b; i++){
+      result += i
+      }
+    return result
+    }
+  
+  }
+
+
 //Sum of the first nth term of Series
 
 function SeriesSum(n){
