79236835ad2711eb89faff060e292edd281307dc
Jackson Payne <codingcerberus@gmail.com>
2022-12-22 23:54:24 +0100

    new answers

--- JS Code wars answers.js
+++ JS Code wars answers.js
@@ -15,6 +15,18 @@ function positiveSum(arr) {
     return sum
   }
 
+//Calculate average
+
+
+function findAverage(array) {
+  if (array.length == 0){
+    return 0
+  } else {
+  return ((array).reduce((a, b) => a + b, 0))/array.length
+    }
+}
+
+
 //Counting sheep...
 
 function countSheeps(arrayOfSheep) {
