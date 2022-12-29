8518f7474ea83d8d179ee49d51d1d29dbf9c6b57
Jackson Payne <codingcerberus@gmail.com>
2022-12-29 23:40:09 +0100

    new answers

--- JS Code wars answers.js
+++ JS Code wars answers.js
@@ -835,3 +835,25 @@ var uniqueInOrder=function(iterable){
     }
     return newArr
   }
+
+
+
+//Count the smiley faces!
+
+function countSmileys(arr) {
+  let count = 0
+  arr.forEach(x => {
+    if ((x.startsWith(':')) || (x.startsWith(';'))){
+      if ((x.endsWith('\)')) || (x.endsWith('D'))){
+        if(x.length === 2){
+        count++
+        } else if (x.length === 3){
+          if ((x[1] == "-") || (x[1] == "~")){
+            count++
+          }
+        }
+      }
+    }
+  })
+  return count
+}
