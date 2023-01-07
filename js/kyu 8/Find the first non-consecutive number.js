389f5aa66bb4005e77abad891107247713f3fb04
Jackson Payne <codingcerberus@gmail.com>
2023-01-07 23:51:19 +0100

    new answer

---
+++ js/kyu 8/Find the first non-consecutive number.js
@@ -0,0 +1,7 @@
+//PREP
+//Params array of numbers, always at least 2 of them, always numbers, always unique, in ascending order, can be positive or negative
+//Returns if arr is consecutive, return null, otherwise return the element not exactly 1 larger then the previous 
+//Example [1, 2, 4, 5] returns 4, [1,2,3,4] returns null
+//Psuedo for loop, check if i + 1 == i++, if not return i++
+
+
