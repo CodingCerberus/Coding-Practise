a1eaeb4020cbdd290653c799b4e354af26d9e42d
Jackson Payne <codingcerberus@gmail.com>
2023-02-28 23:50:25 +0100

    new answer

---
+++ js/kyu 8/Find the position!.js
@@ -0,0 +1,57 @@
+function position(letter){
+    let num = 0
+    if(letter == "a"){
+      num = 1
+    } else if (letter == "b"){
+      num = 2
+    } else if (letter == "c"){
+      num = 3
+    } else if (letter == "d"){
+      num = 4
+    } else if (letter == "e"){
+      num = 5
+    } else if (letter == "f"){
+      num = 6
+    } else if (letter == "g"){
+      num = 7
+    } else if (letter == "h"){
+      num = 8
+    } else if (letter == "i"){
+      num = 9
+    } else if (letter == "j"){
+      num = 10
+    } else if (letter == "k"){
+      num = 11
+    } else if (letter == "l"){
+      num = 12
+    } else if (letter == "m"){
+      num = 13
+    } else if (letter == "n"){
+      num = 14
+    } else if (letter == "o"){
+      num = 15
+    } else if (letter == "p"){
+      num = 16
+    } else if (letter == "q"){
+      num = 17
+    } else if (letter == "r"){
+      num = 18
+    } else if (letter == "s"){
+      num = 19
+    } else if (letter == "t"){
+      num = 20
+    } else if (letter == "u"){
+      num = 21
+    } else if (letter == "v"){
+      num = 22
+    } else if (letter == "w"){
+      num = 23
+    } else if (letter == "x"){
+      num = 24
+    } else if (letter == "y"){
+      num = 25
+    } else if (letter == "z"){
+      num = 26
+    }
+    return `Position of alphabet: ${num}`
+  }
