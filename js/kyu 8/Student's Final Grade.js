78e69667f707fc61c0bb959d63410c34a4d36e50
Jackson Payne <codingcerberus@gmail.com>
2023-04-09 23:33:47 +0200

    new answer

--- js/kyu 8/Student's Final Grade.js
+++ js/kyu 8/Student's Final Grade.js
@@ -1,13 +1,11 @@
 function finalGrade (e, p) {
-    if (e > 90){
-      return p > 10 ? 100 :
-             p >= 5 ? 90  :
-             p >= 2 ? 75  : 0
+    if (e > 90 || p > 10){
+      return 100
     } else if (e > 75){
-      return p >= 5 ? 90 :
-             p >= 2 ? 75 : 0
+      return p > 4 ? 90 :
+             p > 1 ? 75 : 0
     } else if (e > 50){
-      return p >= 2 ? 75 : 0
+      return p > 1 ? 75 : 0
     } else {
       return 0
     }
