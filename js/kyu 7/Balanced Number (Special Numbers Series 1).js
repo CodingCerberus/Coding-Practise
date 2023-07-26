5f44d2bfdc5f689652c17812de75d9cb166ae7e0
Jackson Payne <codingcerberus@gmail.com>
2023-07-26 09:31:47 -0400

    Update Balanced Number (Special Numbers Series 1).js

--- js/kyu 7/Balanced Number (Special Numbers Series 1).js
+++ js/kyu 7/Balanced Number (Special Numbers Series 1).js
@@ -1,35 +1,26 @@
 function balancedNum(number){
   
-  console.log(number)
-  
-  let l = number.length
+  let lSum = 0 
+  let rSum = 0
   number = String(number).split('')
   for(i = 0; i < number.length; i++){
     number[i] = Number(number[i])    
   }
-  
-  
-  
-  if(l < 4){
-  var leftNum = number.slice(0, Math.floor(l / 2))
-  var rightNum = number.slice(-(Math.floor(l / 2)))
-  } else if(l % 2 == 0){
-      var leftNum = number.slice(0, Math.floor(l / 2) - 1)
-      var rightNum = number.slice(-(Math.floor(l / 2)-1))
-  } else if(l % 2 == 1){
-      var leftNum = number.slice(0, Math.floor(l / 2))
-      var rightNum = number.slice(-(Math.floor(l / 2)))
-  }
+  let l = number.length
 
-  console.log(leftNum)
-  console.log(rightNum)
+  console.log(l)
+  for(i = 0; i < l; i++){
+    if(i < ((l / 2) -1)){
+      
+      lSum += number[i]
+    }
+    
+    if(i > (l / 2)){
+      rSum += number[i]
+    }
+  }  
   
-  
-  
-  
-  
-  
-  return l < 3 ? 'Balanced' :
-         leftNum.reduce((a, b)=> a + b, 0) == rightNum.reduce((a, b)=> a + b, 0) ? 'Balanced' : 'Not Balanced'
-  
+  return l < 3 ? 'Balanced' : 
+         lSum == rSum ? 'Balanced' : 'Not Balanced'
+        
 }
