f8ea4960a999777f41b232d4ad976b81f3848fe6
Jackson Payne <codingcerberus@gmail.com>
2023-10-19 14:08:00 -0400

    new answer

---
+++ js/kyu 8/UEFA EURO 2016.js
@@ -0,0 +1,5 @@
+function uefaEuro2016(teams, scores){
+  
+    return `At match ${teams[0]} - ${teams[1]}, ${scores[0] > scores[1] ? teams[0] + " won!" : scores[1] > scores[0] ? teams[1] + " won!" : "teams played draw."}`
+    
+  }
