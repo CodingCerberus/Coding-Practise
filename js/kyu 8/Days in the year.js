beec87f6cf9ff5b937be49424e0ac565e04acbd4
Jackson Payne <codingcerberus@gmail.com>
2023-11-24 00:05:35 -0500

    new answer

---
+++ js/kyu 8/Days in the year.js
@@ -0,0 +1,13 @@
+function yearDays(year){
+  
+    if(year % 100 == 0){
+      return year % 4 == 0 && year % 400 == 0 ? `${year} has 366 days` : `${year} has 365 days`
+    } else if(year % 4 == 0){
+      return `${year} has 366 days`
+    } else if(year == 0){
+      return `${year} has 366 days`
+    } else{
+      return `${year} has 365 days`
+    }
+    
+  }
