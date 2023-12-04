af429c31ff575a0bf2cd183c797cec47183ddf26
Jackson Payne <codingcerberus@gmail.com>
2023-12-03 23:37:22 -0500

    new answer

---
+++ js/kyu 7/Last Survivor.js
@@ -0,0 +1,11 @@
+function lastSurvivor(letters, coords) {
+
+    letters = letters.split('')
+    
+    for(i = 0; i < coords.length; i++){
+      letters.splice(coords[i], 1)
+    }
+    
+    return letters.join('')
+    
+  }
