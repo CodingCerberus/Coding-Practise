a06dbb80d7171122156b84a4054422251ea8d96c
Jackson Payne <codingcerberus@gmail.com>
2024-09-23 01:32:30 -0400

    new answer

---
+++ js/kyu 7/Bingo ( Or Not ).js
@@ -0,0 +1,16 @@
+function bingo(a) {
+  
+    if(a.includes(2)){
+      if(a.includes(7)){
+         if(a.includes(9)){
+           if(a.includes(14)){
+             if(a.includes(15)){
+               return "WIN"
+        }
+       }
+      }
+     }
+    }
+    return "LOSE"
+    
+  }
